e5e2dcf8bec330332d72e1fd753dd1e62075b7f9
frans <franzferdinand.kbarek@gmail.com>
2022-09-12 15:26:21 +0900

    add some res header

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
