261399c36ce8d7a283869c7a18c0faab2e8560f4
frans <franzferdinand.kbarek@gmail.com>
2022-09-12 15:46:22 +0900

    change again cors

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
