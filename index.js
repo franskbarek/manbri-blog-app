964ad219672a4d351a5474b6516d9797eaf67834
frans <franzferdinand.kbarek@gmail.com>
2022-09-12 13:25:51 +0900

    add some allow

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ mongoose
   .then(console.log("💐🌻🌿 Connected to mongodb 🌹🦋🌴🌸"))
   .catch((err) => console.error(err));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
