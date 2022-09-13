3704bda27d2914ae09716ac321e861eb6640d4fd
frans <franzferdinand.kbarek@gmail.com>
2022-09-13 19:03:48 +0900

    set .env

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "https://different-fox-tuxedo.cyclic.app/images/";
+  const PF = "process.env.REACT_APP_BASE_URL/images/";
   console.log(PF);
   return (
     <div className="post">
