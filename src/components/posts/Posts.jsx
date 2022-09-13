3dd94b44aa7e9f8317c4e6df2c411a3e21ac0dbb
frans <franzferdinand.kbarek@gmail.com>
2022-09-13 22:07:23 +0900

    add .env and remove unecessary

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,11 +1,5 @@
 import Post from "../post/Post";
 import "./posts.css";
 export default function Posts({ posts }) {
-  return (
-    <div className="posts">
-      {posts.map((p) => (
-        <Post post={p} />
-      ))}
-    </div>
-  );
+  return <div className="posts">{posts.map((p) => <Post post={p} />).reverse()}</div>;
 }
